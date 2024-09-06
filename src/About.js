94829a772a90d7fb8a245170b06cb64c7b5f86af
Dhineshhoneybee <Dhineshhoneybee@gmail.com>
2024-09-06 13:46:20 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,33 @@
+const About = () => {
+  return (
+      <main className='About'>
+          <h2>About Me</h2>
+          <section>
+              <h3>Dhinesh</h3>
+              <p><strong>Position:</strong> Junior Associate Software Developer at Honeybee Engineering and Innovation Private Limited</p>
+              <p><strong>Education:</strong> MCA</p>
+              <p><strong>Experience:</strong> 1 year in Web Development</p>
+              <p><strong>Skills:</strong></p>
+              <ul>
+                  <li>Full-Stack Development</li>
+                  <li>HTML, CSS, JavaScript</li>
+                  <li>React, Flask, Django</li>
+                  <li>SQL, MySQL</li>
+                  <li>Firebase</li>
+              </ul>
+              <p><strong>Projects:</strong></p>
+              <ul>
+                  <li>HR Web Application</li>
+                  <li>Real Estate Website</li>
+                  <li>React Fetch Api Project</li>
+                  <li>Honeybee Electric Vehicle Ecommerce Project</li>
+                  <li>React Social Media App</li>
+              </ul>
+              <p><strong>Contact:</strong> <a href="mailto:cdhinesh.mca@gmail.com">cdhinesh.mca@gmail.com</a></p>
+              
+          </section>
+      </main>
+  )
+}
+
+export default About;
